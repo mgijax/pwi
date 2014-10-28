87c8b2fa60f498c5d8d0639f6cf551a199d6ac8a
Kevin Stone <Kevin.Stone@jax.org>
2014-10-28 16:48:25 +0000

    Adding tests for synonyms

--- pwi/tests/marker_detail_tests.py
+++ pwi/tests/marker_detail_tests.py
@@ -14,17 +14,19 @@ class MarkerDetailTestCase(unittest.TestCase):
         r = tc.get('/detail/marker/key/10603')
         
         # check Symbol
-        assert 'Kit' in r.data
+        assert 'Kit' in r.data, "check Symbol"
         # check Name
-        assert 'kit oncogene' in r.data
+        assert 'kit oncogene' in r.data, "check Name"
+        # check synonyms
+        assert 'c-KIT' in r.data, "check Synonyms"
         # check status
-        assert 'official' in r.data
+        assert 'official' in r.data, "check Status"
         # check feature type
-        assert 'protein coding gene' in r.data
+        assert 'protein coding gene' in r.data, "check Feature Type"
         # check location string
-        assert 'Chr5' in r.data
+        assert 'Chr5' in r.data, "check Location"
         # check marker detail clip
-        assert 'Some alleles are homozygous lethal' in r.data
+        assert 'Some alleles are homozygous lethal' in r.data, "check Detail Clip Note"
         
     def test_marker_detail_by_id(self):
         # get detail for Kit
@@ -40,7 +42,7 @@ class MarkerDetailTestCase(unittest.TestCase):
         # check status
         assert 'withdrawn' in r.data
         # check location string
-        assert 'ChrUN' in r.data
+        assert 'ChrUN' in r.data, "check Unknown Location"
         
         
 def suite():
