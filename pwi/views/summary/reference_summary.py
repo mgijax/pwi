84c83dd79634c9a721c7ae99cfa4d93259744f9d
Peter Frost <Pete.Frost@jax.org>
2014-11-12 12:54:00 +0000

    adding pubmed submission

--- pwi/views/summary/reference_summary.py
+++ pwi/views/summary/reference_summary.py
@@ -2,14 +2,18 @@ from flask import render_template, request
 from blueprint import summary
 from pwi.util import error_template
 from pwi.model.core import getColumnNames
-#from pwi.forms import MarkerForm
-#from pwi.hunter import marker_hunter, nomen_hunter
+from pwi.forms import ReferenceForm
+from pwi.hunter import reference_hunter
 
 # Routes
     
 @summary.route('/reference',methods=['GET'])
 def referenceSummary():
+
+    form = ReferenceForm(request.args)
     
-    return render_template("summary/reference/reference_summary.html",)
+    references = form.queryReferences()
+
+    return render_template("summary/reference/reference_summary.html", form=form, references=references)
 
     
