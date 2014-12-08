40bd2b29f65611d179d01187dfcdfe2f7e0d4f36
Peter Frost <Pete.Frost@jax.org>
2014-12-08 20:52:57 +0000

    update for allele detail

--- pwi/hunter/allele_hunter.py
+++ pwi/hunter/allele_hunter.py
@@ -1,4 +1,4 @@
-# Used to access marker related data
+# Used to access allele related data
 from pwi.model import Allele
 from pwi import db
 from pwi.model.query import batchLoadAttribute, batchLoadAttributeExists
@@ -12,3 +12,23 @@ def getAlleleByMGIID(id):
     allele = Allele.query.filter_by(mgiid=id).first()
     return allele
 
+def searchAlleles(refs_id=None, mrk_id=None, limit=None):
+    """
+    Perform search for Alleles
+    """
+    
+    query = Allele.query.filter_by(_organism_key=1)
+    
+    if refs_id:
+        query = query.filter(
+#                Allele.explicit_references.any(Reference.jnumid==refs_id)
+        )
+            
+    query = query.order_by(Marker.markerstatus, Marker.symbol)
+    
+    if limit:
+        query = query.limit(limit)
+        
+    alleles = query.all()
+    
+    return markers
