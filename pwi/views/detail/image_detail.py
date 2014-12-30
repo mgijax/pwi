8b57784d25ef1166d4318413304d3fbe72db12a4
Peter Frost <Pete.Frost@jax.org>
2014-12-30 00:43:36 +0000

    initial version

---
+++ pwi/views/detail/image_detail.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from blueprint import detail
+from pwi.hunter import image_hunter
+from pwi.util import error_template
+from pwi import app
+
+# Routes
+
+@detail.route('/image/<string:id>')
+def imageDetailById(id):
+    image = image_hunter.getImageByMGIID(id)
+    if image:
+        return renderImageDetail(image)
+    #return renderImageDetail()
+    
+    return error_template('No image found for ID = %s' % id)
+
+# Helpers
+
+def renderImageDetail(image):
+    
+    return render_template('detail/image_detail.html', image = image)
