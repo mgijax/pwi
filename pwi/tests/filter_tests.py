5c851204347a1c709b9765349844d683e7f73c4d
Peter Frost <Pete.Frost@jax.org>
2015-01-17 03:04:03 +0000

    adding allele symbol and elsevier tag tests

--- pwi/tests/filter_tests.py
+++ pwi/tests/filter_tests.py
@@ -127,6 +127,14 @@ class NotesTagConverterTestCase(unittest.TestCase):
         self.assertEquals('test <a class="" href="%s">Accession</a>' % accurl, 
                           self.convert('test \\Acc(MGI:TEST|Accession|)'))
     
+    def test_notes_tag_elsevier_text(self):
+        self.assertEquals('test Dev Biol 152: 75-88, Yamaguchi TP; Conlon RA; Rossant J, Expression of the fibroblast growth factor receptor FGFR-1/flg during gastrulation and segmentation in the mouse embryo. Copyright 1992', 
+                          self.convert('test \\Elsevier(J:1520||)'))
+
+    def test_notes_tag_allelesymbol_text(self):
+        self.assertEquals('test a', 
+                          self.convert('test \\AlleleSymbol(MGI:1855937|)'))
+
     def test_notes_tag_generic_link(self):
         link = "http://www.google.com"
         self.assertEquals('test <a class="" href="%s" target="_blank">Google</a>' % link, 
