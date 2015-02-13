08a807433492f10bb6276771e34e2e422637de93
Peter Frost <Pete.Frost@jax.org>
2015-02-13 03:46:08 +0000

    iniitial version

---
+++ pwi/forms/result_query.py
@@ -0,0 +1,35 @@
+# Define the query form objects
+from wtforms.form import Form
+from wtforms.fields import *
+from wtforms.widgets import *
+from widgets import *
+from base import *
+from pwi.model import Result
+from pwi.hunter import result_hunter
+
+class ResultForm(Form, MGIForm):
+
+        # possible form parameters
+        marker_id = TextField('Marker MGI ID')
+
+        def _getParams(self):
+
+            params = {}
+
+            if self.marker_id.data:
+                params['marker_id'] = self.marker_id.data
+
+            return params
+        
+
+
+        def queryResults(self):
+
+            results = []
+
+            params = self._getParams()
+            if params:
+                results = result_hunter.searchResults(**params)
+
+            return results
+        
