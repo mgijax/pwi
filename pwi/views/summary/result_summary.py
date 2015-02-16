0aab378f2a86009a247d72fb2353923a402c8a1a
Peter Frost <Pete.Frost@jax.org>
2015-02-16 21:21:16 +0000

    adding tab delim download

--- pwi/views/summary/result_summary.py
+++ pwi/views/summary/result_summary.py
@@ -1,12 +1,12 @@
-from flask import render_template, request
+from flask import render_template, request, Response
 from blueprint import summary
 from pwi import app
 from pwi.hunter import result_hunter
-from pwi.util import error_template
+from pwi.util import error_template, printableTimeStamp
 from pwi.model import Result
 from pwi.forms import ResultForm
 
-# Routes
+### Routes ###
     
 @summary.route('/result',methods=['GET'])
 def resultSummary():
@@ -15,8 +15,17 @@ def resultSummary():
     form = ResultForm(request.args)
     
     return renderResultSummary(form)
+
+@summary.route('/result/download',methods=['GET'])
+def resultSummaryDownload():
+
+    # gather references
+    form = ResultForm(request.args)
     
-# Helpers
+    return renderResultSummaryDownload(form)
+
+    
+### Helpers ###
     
 def renderResultSummary(form):
     
@@ -26,5 +35,49 @@ def renderResultSummary(form):
     return render_template("summary/result/result_summary.html",
                            results=results,
                            form=form,
-                           formArgs=form.argString())
+                           formArgs=form.argString(),
+                           queryString=form.argString())
     
+
+def renderResultSummaryDownload(form):
+    
+    results = form.queryResults()
+
+    # list of data rows
+    resultsForDownload = []
+
+    # add header
+    headerRow = []
+    headerRow.append("Assay ID")
+    headerRow.append("Marker Symbol")
+    headerRow.append("Assay Type")
+    headerRow.append("Mutant Allele")
+    headerRow.append("Age")
+    headerRow.append("Structure")
+    headerRow.append("Detected")
+    resultsForDownload.append(headerRow)
+    
+    for result in results:
+        resultRow = []
+        resultRow.append(result.assay.mgiid)
+        resultRow.append(result.marker.symbol)
+        resultRow.append(result.assay.assaytype)
+        if result.genotype.combination1_cache:
+            resultRow.append(result.genotype.combination1_cache)
+        else: 
+            resultRow.append(" ")
+        resultRow.append(result.age)
+        resultRow.append("TS" + str(result.structure.stage) + ": " + result.structure.printname)
+        resultRow.append(str(result.expressed))
+
+        resultsForDownload.append(resultRow)
+
+    # create a generator for the table cells
+    generator = ("%s\r\n"%("\t".join(row)) for row in resultsForDownload)
+    
+    filename = "result_summary_%s.txt" % printableTimeStamp()
+
+    return Response(generator,
+                mimetype="text/plain",
+                headers={"Content-Disposition":
+                            "attachment;filename=%s" % filename})
