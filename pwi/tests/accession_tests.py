c0848ff074b36efe2fdc9624629a85dde39bb574
Peter Frost <Pete.Frost@jax.org>
2015-03-18 18:02:02 +0000

    adding vocab terms

--- pwi/tests/accession_tests.py
+++ pwi/tests/accession_tests.py
@@ -75,6 +75,34 @@ class AccessionQueryTestCase(unittest.TestCase):
                    follow_redirects=True)
         assert 'Otx2' in r.data, "Check Partial Allelic Combination"
 
+    # OMIM term
+    def test_accession_omim(self):
+        # get detail for omim
+        r = tc.get('/accession/305400', 
+                   follow_redirects=True)
+        assert 'Aarskog-Scott Syndrome' in r.data, "Check Term Name"
+
+    # MP Term
+    def test_accession_mp(self):
+        # get detail for MP term
+        r = tc.get('/accession/MP:0005369', 
+                   follow_redirects=True)
+        assert 'muscle phenotype' in r.data, "Check Term Name"
+
+    # GO Term
+    def test_accession_go(self):
+        # get detail for GO term
+        r = tc.get('/accession/GO:0009055', 
+                   follow_redirects=True)
+        assert 'electron carrier activity' in r.data, "Check Term Name"
+
+    # EMAPS Term
+    def test_accession_emaps(self):
+        # get detail for EMAPS term
+        r = tc.get('/accession/EMAPS:1610523', 
+                   follow_redirects=True)
+        assert 'heart' in r.data, "Check Term Name"
+
 
     #
     # FORM & SUMMARY
@@ -82,7 +110,7 @@ class AccessionQueryTestCase(unittest.TestCase):
 
     def test_accession_form_submit(self):
         # use different types of IDs
-        r = tc.get('/accession/query?ids=MGI:96677, MGI:1879167, MGI:47013, MGI:2665324, MGI:5582113, MGI:3587216, MGI:3530308, MGI:2166442, J:2', 
+        r = tc.get('/accession/query?ids=MGI:96677, MGI:1879167, MGI:47013, MGI:2665324, MGI:5582113, MGI:3587216, MGI:3530308, MGI:2166442, J:2, 305400, MP:0005369, GO:0009055, EMAPS:1610523', 
                    follow_redirects=True)
         
         # check that each ID is in summary
@@ -95,6 +123,10 @@ class AccessionQueryTestCase(unittest.TestCase):
         assert 'MGI:3530308' in r.data, "Check Allele"
         assert 'MGI:2166442' in r.data, "Check Genotype"
         assert 'J:2' in r.data, "Check Reference"
+        assert '305400' in r.data, "Check OMIM"
+        assert 'MP:0005369' in r.data, "Check MP"
+        assert 'GO:0009055' in r.data, "Check GO"
+        assert 'EMAPS:1610523' in r.data, "Check EMAPS"
        
         
 def suite():
