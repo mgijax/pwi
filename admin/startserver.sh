bee0cfd3991b9afc72073e4e3bd51a10f23e3341
Kevin Stone <kevinrstone@gmail.com>
2015-12-17 11:56:03 -0500

    adding error log for startserver.sh

--- admin/startserver.sh
+++ admin/startserver.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-python prodserver.py --port=$SERVER_PORT 1>/dev/null 2>/dev/null &
+ERROR_LOG=startserver.sh.error
+rm -f $ERROR_LOG
+python prodserver.py --port=$SERVER_PORT 1>/dev/null 2>$ERROR_LOG &
+
+sleep 1
+
+if [[ -s $ERROR_LOG ]] ; then
+	cat $ERROR_LOG
+fi ;
+
+echo "stderr log = $ERROR_LOG"
