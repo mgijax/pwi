5afe4ec7468e85bf319565842ba69fad7e12b36f
PeteFrost <pete.frost@jax.org>
2016-02-10 14:08:49 -0500

    visual tweaks

--- pwi/views/edit/emap_browser.py
+++ pwi/views/edit/emap_browser.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, Response
 from blueprint import edit
 from mgipython.util import error_template
+from pwi import app
+from pwi.forms import EMAPAForm
 #from mgipython.model import Foo
 #from pwi.hunter import foo_hunter
 #from pwi.forms import FooForm
@@ -19,5 +21,16 @@ def emapBrowser():
 #    return render_template("edit/emap_browser.html",
 #                           queryString=form.argString() )
     
+@edit.route('/emapTermResults',methods=['GET'])
+def emapTermResults():
+
+    form = EMAPAForm(request.args)
+
+    app.logger.debug("form = %s " % form.argString())
+
+    terms = form.queryEMAPATerms()
+        
+    return render_template( "edit/emap_term_results.html",
+        terms=terms)
     
     
