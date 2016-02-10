affc5ffe03e8fb9787a853ab804befce4eaf2c18
PeteFrost <pete.frost@jax.org>
2016-02-10 12:15:40 -0500

    adding emapa import

--- pwi/forms/__init__.py
+++ pwi/forms/__init__.py
@@ -2,6 +2,7 @@ from allele_query import *
 from antibody_query import *
 from experiment_query import *
 from gxd_query import *
+from emapa_query import *
 from image_query import *
 from marker_query import *
 from probe_query import *
