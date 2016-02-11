1bc7629ae440756d04706b9fa31646f7712c37e1
Kevin Stone <kevinrstone@gmail.com>
2016-02-11 11:35:27 -0500

    1) refactored templates 2) added js and css files for emapa 3) added term detail endpoint

---
+++ pwi/static/edit/emapBrowser.js
@@ -0,0 +1,6 @@
+(function(){
+	"use strict";
+	
+	// javascript for emapBrowser
+	
+})();
--- pwi/views/edit/emap_browser.py
+++ pwi/views/edit/emap_browser.py
@@ -3,6 +3,7 @@ from blueprint import edit
 from mgipython.util import error_template
 from pwi import app
 from pwi.forms import EMAPAForm
+from pwi.hunter import vocterm_hunter
 #from mgipython.model import Foo
 #from pwi.hunter import foo_hunter
 #from pwi.forms import FooForm
@@ -17,12 +18,16 @@ def emapBrowser():
     #form = FooForm(request.args)
 
     
-    return render_template( "edit/emap_browser.html" )
+    return render_template( "edit/emapa/emap_browser.html" )
 #    return render_template("edit/emap_browser.html",
 #                           queryString=form.argString() )
     
+    
 @edit.route('/emapTermResults',methods=['GET'])
 def emapTermResults():
+    """
+    Results summary to be injected into emapBrowser page
+    """
 
     form = EMAPAForm(request.args)
 
@@ -30,7 +35,41 @@ def emapTermResults():
 
     terms = form.queryEMAPATerms()
         
-    return render_template( "edit/emap_term_results.html",
+    return render_template( "edit/emapa/emap_term_results.html",
         terms=terms)
     
     
+@edit.route('/emapTermDetail/key/<int:key>',methods=['GET'])    
+def emapTermDetailByKey(key):
+    """
+    EMAPA/S term detail to be injected into emapBrowser page
+    
+    NOTE: key based url primarily for SE testing
+    """
+
+    term = vocterm_hunter.getVocTermByKey(key)
+    if term:
+        return renderEmapaTermDetailSection(term)
+    
+    return error_template('No term found for _term_key = %d' % key)
+    
+    
+@edit.route('/emapTermDetail/<string:id>',methods=['GET'])    
+def emapTermDetailById(id):  
+    """
+    EMAPA/S term detail to be injected into emapBrowser page
+    """
+    
+    term = vocterm_hunter.getVocTermByPrimaryID(id)
+    if term:
+        return renderEmapaTermDetailSection(term)
+
+    return error_template('No term found for id = %s' % id)
+
+    
+def renderEmapaTermDetailSection(term):
+    
+    return render_template( "edit/emapa/emapa_term_detail.html",
+            term=term)
+
+
