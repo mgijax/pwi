2cbda8d97c338d2662a63aa3828b86939ac41330
Kevin Stone <kevinrstone@gmail.com>
2016-02-11 12:07:37 -0500

    adding term detail happy path test

--- tests/edit/emapa_browser_tests.py
+++ tests/edit/emapa_browser_tests.py
@@ -84,6 +84,28 @@ class EMAPASearchTestCase(unittest.TestCase):
         assert 'heart' in r.data, "Check term returned"
         assert 'nervous system' in r.data, "Check term returned"
         
+        
+class EMAPATermDetailTestCase(unittest.TestCase):
+    """
+    Test EMAPA term detail
+    """
+    
+    ### Helpers ###
+    
+    # NOTE: no logins needed for searching
+            
+    ### Tests ###
+            
+    def test_emapa_basic_term_detail(self):
+        # detail for metanephros
+        r = tc.get('/edit/emapTermDetail/EMAPA:17373')    
+        
+        assert 'metanephros' in r.data, "Check term returned"
+        assert '18-28' in r.data, "Check TS range"
+        assert 'EMAPA:17373' in r.data, "Check ID"
+        assert 'kidney' in r.data, "Check synonym"
+        assert 'is-a' in r.data, "Check parent label"
+        assert 'urinary system' in r.data, "Check parent term"
     
     
 class EMAPAClipboardTestCase(unittest.TestCase):
@@ -123,6 +145,7 @@ class EMAPAClipboardTestCase(unittest.TestCase):
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(EMAPASearchTestCase))
+    suite.addTest(unittest.makeSuite(EMAPATermDetailTestCase))
     suite.addTest(unittest.makeSuite(EMAPAClipboardTestCase))
     return suite
 
