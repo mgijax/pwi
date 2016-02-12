7f095bb37c3de4c991a67d05a493664610cc9e6e
PeteFrost <pete.frost@jax.org>
2016-02-11 21:29:45 -0500

    tweaks to display and submittal

--- pwi/static/edit/emapBrowser.js
+++ pwi/static/edit/emapBrowser.js
@@ -1,15 +1,16 @@
 (function(){
 	"use strict";
-	
+
 	// javascript for emapBrowser
+
 	/*
 	 * Need to set following before use
-	 * 
+	 *
 	 * EMAPA_SEARCH_URL, EMAPA_DETAIL_URL
 	 */
-	
+
 	var TERM_DETAIL_ID = "termDetailsArea";
-	
+
 	/*
 	 * Click on a parent term in the term detail section
 	 */
@@ -17,7 +18,7 @@
 		var parentId = $(this).attr("data_id");
 		MGIAjax.loadContent(EMAPA_DETAIL_URL + parentId, TERM_DETAIL_ID, setupTermDetailsEvents);
 	};
-	
+
 	/*
 	 * Setup term detail events:
 	 * Need to reconfigure javascript events listeners for newly loaded content
@@ -25,17 +26,17 @@
 	var setupTermDetailsEvents = function(){
 		$(".termDetailParent").click(termParentClick);
 	};
-	
-	
+
+
 	/*
 	 * Click on a search result term
 	 */
 	var searchResultClick = function(){
 		var termId = $(this).attr("data_id");
-		
+
 		MGIAjax.loadContent(EMAPA_DETAIL_URL + termId, TERM_DETAIL_ID, setupTermDetailsEvents);
 	};
-	
+
 	/*
 	 * Setup term search events:
 	 * Need to reconfigure javascript event listeners for newly loaded content
@@ -43,7 +44,7 @@
 	var setupTermSearchEvents = function(){
 		$(".termSearchResult").click(searchResultClick);
 	};
-	
+
 	/*
 	 * Handle query form submit
 	 */
@@ -55,5 +56,23 @@
 
 	    return  false;
 	});
-	
+
+	$(document).ready(function () {
+
+		/*
+		 * Setup horizontal resizing of page
+		 */
+	    $(".leftContainer").resizable({
+	        handles: 'e',
+	        minWidth: 280,
+	        maxWidth: 800,
+	        resize: function () {
+	            $('.leftContainer').css('width', $('.leftContainer').outerWidth() * 100 / $(window).innerWidth() + '%');
+	            $('.rightContainer').css('width', 99 - ($('.leftContainer').outerWidth() * 100 / $(window).innerWidth()) + '%');
+	        }
+	    });
+		$('#termSearch').val(TERM_SEARCH);
+		$('#termSearchForm').submit();	});
+
+
 })();
