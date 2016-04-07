9aaa702ce62515a8863f0f489a24e23b885e44a6
pf <peter.frost@jax.org>
2016-04-07 08:34:22 -0400

    Adding specimen summary

---
+++ pwi/forms/specimen_query.py
@@ -0,0 +1,34 @@
+# Define the query form objects
+from wtforms.form import Form
+from wtforms.fields import *
+from wtforms.widgets import *
+from widgets import *
+from base import *
+from pwi.hunter import specimen_hunter
+
+class SpecimenForm(Form, MGIForm):
+
+        # possible form parameters
+        jnum = TextField('Reference JNum')
+        
+        def _getParams(self):
+            params = {}
+            if self.jnum.data:
+                params['jnum'] = self.jnum.data
+            return params
+        
+        
+        def querySpecimens(self):
+            """
+            Returns specimens based on the form parameters provided.
+            Return nothing if no viable parameters were entered.
+            """
+            specimens = []
+
+            params = self._getParams()
+            if params:
+                specimens = specimen_hunter.searchSpecimens(**params)
+                
+            return specimens
+        
+        
