2422b66cfa72c5de403c15828c6aea18e979b619
Kevin Stone <kevinrstone@gmail.com>
2016-04-28 12:17:10 -0400

    add assay type column to imagepane summary

--- pwi/views/summary/imagepane_summary.py
+++ pwi/views/summary/imagepane_summary.py
@@ -4,6 +4,7 @@ from pwi import app
 from pwi.hunter import image_hunter
 from mgipython.util import error_template
 from mgipython.model import NOM_Marker
+from mgipython.model.query import batchLoadAttribute
 from pwi.forms import ImagepaneForm
 
 # Routes
@@ -22,7 +23,16 @@ def renderImagepaneSummary(form):
     
     # gather lists of image pages via hunter
     images = form.searchImages()
-    print images.__len__()
+    
+    # batch load relationships with more efficient SQL
+    batchLoadAttribute(images, 'imagepanes')
+    batchLoadAttribute(images, 'imagepanes.insituresults')
+    batchLoadAttribute(images, 'imagepanes.insituresults.specimen')
+    batchLoadAttribute(images, 'imagepanes.insituresults.specimen.assay')
+    batchLoadAttribute(images, 'imagepanes.insituresults.specimen.assay.marker')
+    batchLoadAttribute(images, 'imagepanes.gel_assays')
+    batchLoadAttribute(images, 'imagepanes.gel_assays.marker')
+    
     return render_template("summary/imagePane/imagepane_summary.html",
                            form=form,
                            images=images,
