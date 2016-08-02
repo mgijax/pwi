972aed4ef1add8118b3f520c472ad681a896f5c0
Olin Blodgett <nilo@nilosplace.net>
2016-08-02 09:28:40 -0400

    Debug Start Script

--- admin/redbob_startserver.sh
+++ admin/redbob_startserver.sh
@@ -5,4 +5,5 @@ source ../Configuration
 ERROR_LOG=startserver.sh.error
 rm -f $ERROR_LOG
 #python prodserver.py --port=$SERVER_PORT 1>$PWI/logs/stdout.log 2>$ERROR_LOG &
-nohup python prodserver.py --port=7001  &
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/python/lib
+python prodserver.py --port=7001
