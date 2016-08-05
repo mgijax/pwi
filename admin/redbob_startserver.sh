6b0b5c50b027e67cad5c32240e3ff2af866c4227
Olin Blodgett <nilo@nilosplace.net>
2016-08-05 14:22:44 -0400

    Removed server port as its getting if from the env

--- admin/redbob_startserver.sh
+++ admin/redbob_startserver.sh
@@ -5,5 +5,5 @@ source ../Configuration
 ERROR_LOG=startserver.sh.error
 rm -f $ERROR_LOG
 #python prodserver.py --port=$SERVER_PORT 1>$PWI/logs/stdout.log 2>$ERROR_LOG &
-nohup python prodserver.py --port=7001  &
+nohup python prodserver.py &
 
