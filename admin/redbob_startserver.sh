c7afbd869325c7cb4e4d91cd51b7c1c0984b9dd2
Olin Blodgett <nilo@nilosplace.net>
2016-08-05 14:31:19 -0400

    Prevent hanging when starting from command line

--- admin/redbob_startserver.sh
+++ admin/redbob_startserver.sh
@@ -3,7 +3,5 @@
 source ../Configuration
 
 ERROR_LOG=startserver.sh.error
-rm -f $ERROR_LOG
-#python prodserver.py --port=$SERVER_PORT 1>$PWI/logs/stdout.log 2>$ERROR_LOG &
-nohup python prodserver.py &
 
+nohup python prodserver.py > $ERROR_LOG 2> $ERROR_LOG < /dev/null &
