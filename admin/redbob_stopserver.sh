ed61ff7ff4a7a4a38150d6217b3704f24dbf6e1c
Olin Blodgett <nilo@nilosplace.net>
2016-08-05 14:55:11 -0400

    Added info about server_port

--- admin/redbob_stopserver.sh
+++ admin/redbob_stopserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Server Port here is only used to identify the running process not set the port the server is running on
 pid=`pgrep -f "python prodserver.py --port=$SERVER_PORT"`
 if [ "$pid" = "" ]; then
         echo "No process found"
