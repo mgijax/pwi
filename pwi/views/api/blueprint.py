60d31f35d8490ec03c2e66fa40555f8f3203fad6
Olin Blodgett <nilo@nilosplace.net>
2016-08-17 09:55:38 -0400

    Added the initial experiment API

--- pwi/views/api/blueprint.py
+++ pwi/views/api/blueprint.py
@@ -5,11 +5,19 @@ from mgipython.error import NotFoundError, InvalidPermissionError
 import psycopg2
 
 # Define the blueprint for all the views in this directory
+app.config.SWAGGER_UI_JSONEDITOR = True
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 # turn blueprint into Flask Restplus object
-api = Api(api_bp)
+api = Api(api_bp,
+    version='1.0',
+    title='PWI API Spec',
+    description='This API is for the PWI to interact with the database. This software is managed by the following repository at GitHub <a href=\"https://github.com/mgijax/pwi\">PWI</a>',
+    contact="Mouse Gnome Informatics",
+    contact_email="mgiadmin@jax.org",
+    contact_url="http://www.informatics.jax.org"
+)
 
 @api_bp.after_request
 def api_after_request(response):
@@ -27,7 +35,6 @@ def handle_server_error(error):
     All exceptions get 500 by default
     """
     app.logger.exception(error)
-    app.logger.info("hello I am here!!!!")
     return error_response_as_json(error, 500)
 
 
@@ -64,6 +71,9 @@ api.add_namespace(emapa_ns)
 
 from gxdindex_api import api as gxdindex_ns
 api.add_namespace(gxdindex_ns)
+
+from gxd_ht_experiment_api import api as gxd_ht_experiment_api
+api.add_namespace(gxd_ht_experiment_api)
 
 from reference_api import api as reference_ns
 api.add_namespace(reference_ns)
--- pwi/views/api/gxd_ht_experiment_api.py
+++ pwi/views/api/gxd_ht_experiment_api.py
@@ -1,6 +1,5 @@
-from flask import render_template, abort, url_for
-from flask_restful import fields, marshal_with, reqparse, Resource, Api
-from flask_restful_swagger import swagger
+from flask import request, render_template, abort, url_for
+from flask_restplus import fields, Namespace, reqparse, Resource, Api
 from flask_login import current_user
 from blueprint import api
 from mgipython.util import error_template
@@ -8,30 +7,64 @@ from mgipython.model import GxdHTExperiment
 from mgipython.service.gxd_ht_experiment_service import GxdHTExperimentService
 from pwi import app
 
-class GxdHTExperimentResource(Resource):
-    "Describing elephants"
-    @swagger.operation(
-        notes='some really good notes',
-        responseClass=GxdHTExperiment.__name__,
-        nickname='upload',
-        parameters=[{
-              "name": "body",
-              "description": "blueprint object that needs to be added. YAML.",
-              "required": True,
-              "allowMultiple": False,
-              "dataType": GxdHTExperiment.__name__,
-              "paramType": "body"
-        }],
-        responseMessages=[{
-            "code": 201,
-            "message": "Created. The URL of the created blueprint should be in the Location header"
-        }, {
-            "code": 405,
-            "message": "Invalid input"
-        }]
-    )
-    def get(self, todo_id): 
-        return "Test"
+api = Namespace('gxdhtexperiment', description='GXD HT Experiment API actions')
+
+gxdhtexperiment_parser = reqparse.RequestParser()
+gxdhtexperiment_parser.add_argument('name')
+gxdhtexperiment_parser.add_argument('description')
+
+gxdhtexperiment_model = api.model('GxdHTExperiment', {
+    'name': fields.String,
+	 'description': fields.String
+})
+
+@api.route('/', endpoint='gxdhtexperiment-create-resource')
+class GxdHTExperimentCreateResource(Resource):
+
+    gxdhtexperiment_service = GxdHTExperimentService()
+
+    @api.expect(gxdhtexperiment_model)
+    def post(self):
+        experiment = self.gxdhtexperiment_service.create(gxdhtexperiment_model)
+        return experiment.serialize()
+
+@api.route('/<int:key>', endpoint='gxdhtexperiment-modify-resource')
+@api.param('key', 'mgd.gxd_ht_experiment._experiment_key')
+class GxdHTExperimentModifyResource(Resource):
+
+    gxdhtexperiment_service = GxdHTExperimentService()
+
+    @api.expect(gxdhtexperiment_model)
+    def put(self, key):
+        experiment = self.gxdhtexperiment_service.modify(key, gxdhtexperiment_model)
+        return experiment.serialize()
+
+    def get(self, key):
+        experiment = self.gxdhtexperiment_service.get_by_key(key)
+        return experiment.serialize()
+
+    def delete(self, key):
+        experiment = self.gxdhtexperiment_service.delete(key)
+        return experiment.serialize()
 
 
-api.add_resource(GxdHTExperiment, '/gxdhtexp')
+@api.route('/search', endpoint='gxdhtexperiment-search-resource')
+class GxdHTExperimentSearchResource(Resource):
+
+    gxdhtexperiment_service = GxdHTExperimentService()
+
+    @api.doc(description='Description')
+    @api.expect(gxdhtexperiment_parser)
+    def get(self):
+        experiments = self.gxdhtexperiment_service.search(gxdhtexperiment_parser)
+        return experiment.serialize()
+
+    @api.expect(gxdhtexperiment_model)
+    def post(self):
+        
+        experiment = self.gxdhtexperiment_service.search(gxdhtexperiment_model)
+        return experiment.serialize()
+
+
+
+
