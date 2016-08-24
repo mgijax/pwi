2bc4ae357173b1096dade08ebb573707c1fc920b
Olin Blodgett <nilo@nilosplace.net>
2016-08-24 11:45:18 -0400

    Added query time logging to output

--- pwi/__init__.py
+++ pwi/__init__.py
@@ -76,9 +76,21 @@ except:
 from login.literalquery import literalquery
 from datetime import datetime
 import sqlparse
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from sqlalchemy.event import listens_for
 from sqlalchemy.orm.query import Query
 import traceback
+import time
+
+@event.listens_for(Engine, "before_cursor_execute")
+def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    conn.info.setdefault('query_start_time', []).append(time.time())
+
+@event.listens_for(Engine, "after_cursor_execute")
+def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    total = time.time() - conn.info['query_start_time'].pop(-1)
+    app.logger.debug("Total Time: %f", total)
 
 
 @listens_for(Query, "before_compile")
