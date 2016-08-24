38ad29aa7d59af34ef8ff7c79786d4a1373d18df
Olin Blodgett <nilo@nilosplace.net>
2016-08-24 11:18:58 -0400

    Refactored and orginaized code

--- pwi/static/app/app.modeule.js
+++ pwi/static/app/app.modeule.js
@@ -1,10 +1,12 @@
 (function() {
   'use strict';
   angular.module('PWI', [
+    'pwi',
     'pwi.gxd',
   ]);
 
 // define root modules & dependencies
+  angular.module('pwi', ['ui.bootstrap', 'ui.bootstrap.tpls']);
   angular.module('pwi.gxd', ['formly', 'formlyBootstrap', 'ui.bootstrap', 'ui.bootstrap.tpls', 'ngResource', 'angularSpinner']);
 
 })();
