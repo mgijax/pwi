9dd3b944b88ebc8a53997c2fd71f3b25dd0855bb
Olin Blodgett <nilo@nilosplace.net>
2016-10-24 16:05:23 -0400

    Added fix for sorting by name field

--- pwi/static/app/filters/SortFilter.js
+++ pwi/static/app/filters/SortFilter.js
@@ -7,14 +7,15 @@
 				if(reverse) {
 					neg = "-";
 				}
-
-				if("raw_sample" in array[0] && predicteObject in array[0].raw_sample) {
-					array.sort(naturalSortService.naturalSort2("raw_sample", neg + predicteObject));
-					return array;
-				}
-				if("sample_domain" in array[0] && predicteObject in array[0].sample_domain) {
-					array.sort(naturalSortService.naturalSort2("sample_domain", neg + predicteObject));
-					return array;
+				if(predicteObject != "name") {
+					if("raw_sample" in array[0] && array[0].raw_sample && predicteObject in array[0].raw_sample) {
+						array.sort(naturalSortService.naturalSort2("raw_sample", neg + predicteObject));
+						return array;
+					}
+					if("sample_domain" in array[0] && array[0].sample_domain && predicteObject in array[0].sample_domain) {
+						array.sort(naturalSortService.naturalSort2("sample_domain", neg + predicteObject));
+						return array;
+					}
 				}
 				array.sort(naturalSortService.naturalSort(neg + predicteObject));
 				return array;
