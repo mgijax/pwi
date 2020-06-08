b3f53e55d2a0d4e5fe2194b4c981cf24bc20c5e8
Lori Corbani <lori.corbani@jax.org>
2020-06-08 12:24:00 -0400

    TR13204/Infrastructure project/Alleles

--- pwi/static/app/edit/allele/AlleleController.js
+++ pwi/static/app/edit/allele/AlleleController.js
@@ -301,6 +301,7 @@
 				AlleleUpdateAPI.update(vm.apiDomain, function(data) {
 					if (data.error != null) {
 						alert("ERROR: " + data.error + " - " + data.message);
+						loadObject();
 					}
 					else {
 						loadObject();
