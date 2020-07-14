9e122750d68cbe717f2e2320d17ee0d975c0a337
Lori Corbani <lori.corbani@jax.org>
2020-07-14 07:13:55 -0400

    TR13204/Infrastructure project/nonmutantcellline

---
+++ pwi/static/app/edit/nonmutantcellline/NonMutantCellLineService.js
@@ -0,0 +1,69 @@
+(function() {
+	'use strict';
+	angular.module('pwi.nonmutantcellline')
+		.factory('NonMutantCellLineSearchAPI',		NonMutantCellLineSearchAPIResource)
+		.factory('NonMutantCellLineGetAPI',		NonMutantCellLineGetAPIResource)
+                .factory('NonMutantCellLineCreateAPI',          NonMutantCellLineCreateAPIResource)
+		.factory('NonMutantCellLineUpdateAPI',		NonMutantCellLineUpdateAPIResource)
+		.factory('NonMutantCellLineDeleteAPI',		NonMutantCellLineDeleteAPIResource)
+		.factory('NonMutantCellLineTotalCountAPI',	NonMutantCellLineTotalCountAPIResource)
+		.factory('NonMutantCellLineMCLCountAPI',	NonMutantCellLineMCLCountAPIResource)
+		;
+
+	// object summary search
+	function NonMutantCellLineSearchAPIResource($resource, JAVA_API_URL) {
+		return $resource(JAVA_API_URL + 'cellline/searchParentCellLines', {}, {
+			'search': { method: 'POST', isArray: true }
+		});
+	}
+
+	// object retrieval by key
+	function NonMutantCellLineGetAPIResource($resource, JAVA_API_URL) {
+		return $resource(JAVA_API_URL + 'cellline/:key', {}, {
+			'': { method: 'JSONP' } 
+		});
+	}
+
+	// create
+	function NonMutantCellLineCreateAPIResource($resource, JAVA_API_URL, USERNAME) {
+		return $resource(JAVA_API_URL + 'cellline', {},
+			{'create': { method: 'POST', 
+			headers: { 'api_access_token': access_token, 'username': USERNAME } 
+			}
+		});
+	}	
+
+	// update
+	function NonMutantCellLineUpdateAPIResource($resource, JAVA_API_URL, USERNAME) {
+		return $resource(JAVA_API_URL + 'cellline', {},
+			{'update': { method: 'PUT', 
+			 headers: { 'api_access_token': access_token, 'username': USERNAME } 
+			}
+		});
+	}	
+
+	// delete
+	function NonMutantCellLineDeleteAPIResource($resource, JAVA_API_URL, USERNAME) {
+		return $resource(JAVA_API_URL + 'cellline/:key', {},
+			{'delete': { method: 'DELETE', 
+			headers: { 'api_access_token': access_token, 'username': USERNAME } 
+			}
+		});
+	}	
+
+	// total number of records
+	function NonMutantCellLineTotalCountAPIResource($resource, JAVA_API_URL) {
+		return $resource(JAVA_API_URL + 'cellline/getParentCellLineCount', {}, {
+			'getParentCellLineCount': { method: 'JSONP' } 
+		});
+	}	
+	
+	// mcl number of records by parent cell line key
+	function NonMutantCellLineMCLCountAPIResource($resource, JAVA_API_URL) {
+		return $resource(JAVA_API_URL + 'cellline/getMCLCountByParentCellLine', {}, {
+			'search': { method: 'POST' } 
+		});
+	}	
+	
+})();
+
