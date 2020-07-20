98f2045556d5a86271b6cb790ffac258b4aa8bc4
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2020-07-20 16:46:45 -0400

    fix to summary scrolling

--- pwi/static/app/edit/antigen/AntigenController.js
+++ pwi/static/app/edit/antigen/AntigenController.js
@@ -424,7 +424,7 @@
                         console.log("scrollToObject()");
 			$q.all([
 			   FindElement.byId("resultTableWrapper"),
-			   FindElement.byQuery("#resultsTable .resultsTableSelectedRow")
+			   FindElement.byQuery("#resultsTable .SelectedRow")
 			 ]).then(function(elements) {
 				 var table = angular.element(elements[0]);
 				 var selected = angular.element(elements[1]);
