68ba4a19e870561d39be6014cb2c89c7f54dc178
Lori Corbani <lori.corbani@jax.org>
2021-05-18 08:40:13 -0400

    fix Antibody/validateTIssue/document focus

--- pwi/static/app/edit/antibody/AntibodyController.js
+++ pwi/static/app/edit/antibody/AntibodyController.js
@@ -567,7 +567,7 @@
 
                         }, function(err) {
                                 pageScope.handleError(vm, "API ERROR: ValidateTissueAPI.search");
-                                document.getElementById(id).focus();
+                                document.getElementById("tissue").focus();
                         });
                 }
                 
