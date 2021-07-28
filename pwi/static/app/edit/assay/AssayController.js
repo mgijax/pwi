5860cb3367fc2e783d4e0926d3d4c489f60b5f85
Lori Corbani <lori.corbani@jax.org>
2021-07-28 08:48:58 -0400

    yaks/assay

--- pwi/static/app/edit/assay/AssayController.js
+++ pwi/static/app/edit/assay/AssayController.js
@@ -230,10 +230,12 @@
 				}
 				else {
 					vm.apiDomain = data.items[0];
-                                               vm.selectedIndex = vm.results.length;
-                                               vm.results[vm.selectedIndex] = [];
-                                               vm.results[vm.selectedIndex].assayKey = vm.apiDomain.assayKey;
-					vm.results[vm.selectedIndex].assayDisplay = vm.apiDomain.assayDisplay;
+                                        vm.selectedIndex = vm.results.length;
+			                var item = {
+				                "assayKey": vm.apiDomain.assayKey,
+				                "assayDisplay": vm.apiDomain.assayDisplay
+			                }
+                                        vm.results[vm.selectedIndex] = item;
 					loadObject();
 					refreshTotalCount();
 				}
