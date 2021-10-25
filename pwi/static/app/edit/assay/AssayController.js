f27909d1df8ccb9b9f5a619b68c3ace15ca148a1
Lori Corbani <lori.corbani@jax.org>
2021-10-25 10:33:01 -0400

    yaks/assay

--- pwi/static/app/edit/assay/AssayController.js
+++ pwi/static/app/edit/assay/AssayController.js
@@ -4167,6 +4167,23 @@
                         lastSummaryObject();
                 }
 
+		// linkout to assay detail
+                function assayDetailLink() {
+			console.log("assayDetailLink: " + vm.apiDomain.accID);
+
+                        var assayUrl = pageScope.PWI_BASE_URL + "edit/assaydetail/?id=";
+			var params = [];
+
+			if (vm.apiDomain.accID == "") {
+                                return;
+			}
+
+			assayUrl = assayUrl + vm.apiDomain.accID
+			console.log(assayUrl);
+
+                        window.open(assayUrl, '_blank');
+                }
+
 		/////////////////////////////////////////////////////////////////////
 		// Angular binding of methods 
 		/////////////////////////////////////////////////////////////////////		
@@ -4257,6 +4274,9 @@
 
                 // duplicate prep, partial, all
                 $scope.duplicateAssay = duplicateAssay;
+
+                // link-outs
+                $scope.assayDetailLink = assayDetailLink;
 
                 // setting columns as hideable
                 $scope.showAllColumns = showAllColumns;
