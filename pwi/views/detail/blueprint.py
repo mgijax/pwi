94a08ae19095fa9dffb1ec92c8f68d9d2841bf71
Lori Corbani <lori.corbani@jax.org>
2022-01-06 09:59:10 -0500

    wts2-477/TR13225--change pWI image detail from LATIN9 to UTF8

--- pwi/views/detail/blueprint.py
+++ pwi/views/detail/blueprint.py
@@ -7,7 +7,6 @@ detail = Blueprint('detail', __name__, url_prefix='/detail')
 from . import allele_detail
 from . import antibody_detail
 from . import experiment_detail
-from . import image_detail
 from . import marker_detail
 from . import probe_detail
 from . import vocterm_detail
