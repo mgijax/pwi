debf16d367c5339e4b90e63469dc656125aa01e3
Lori Corbani <lori.corbani@jax.org>
2022-06-09 14:57:43 -0400

    replace API_PATH with JAVA_API_URL/ActualDbGet

--- pwi/static/app/edit/triage/LitTriageController.js
+++ pwi/static/app/edit/triage/LitTriageController.js
@@ -22,11 +22,11 @@
 			ReferenceUpdateAPI,
 			ReferenceDeleteAPI,
 			ReferenceBatchRefUpdateTagAPI,
-			ActualDbSearchAPI,
 			ReferenceAlleleAssocAPI,
 			ReferenceMarkerAssocAPI,
 			ReferenceStrainAssocAPI,
 			JournalAPI,
+			ActualDbGetAPI,
 			// global resource APIs
 			MGIRefAssocTypeSearchAPI,
 			ValidateAlleleAPI,
@@ -91,22 +91,10 @@
 		
 		// load the actual db values from DB for linking purposes
 		function loadActualDbValues() {
-
 			// URL for DOI link
-			ActualDbSearchAPI.get(
-			  {_actualdb_key:'65'}, 
-			  function(data) {
-				vm.actualDbData = data.items;
-				vm.doi_url = data.items[0].url;
-			});
-			
+			ActualDbGetAPI.get({ key: '65' }, function(data) { vm.doi_url = data.actualDBs[0].url; });
 			// URL for pubmed links
-			ActualDbSearchAPI.get(
-			  {_actualdb_key:'37'}, 
-			  function(data) {
-				vm.actualDbData = data.items;
-				vm.pubmed_url = data.items[0].url;
-			});
+			ActualDbGetAPI.get({ key: '29' }, function(data) { vm.pubmed_url = data.actualDBs[0].url; });
 		}
 		
 		// load the vocab choices for reference type drop list
