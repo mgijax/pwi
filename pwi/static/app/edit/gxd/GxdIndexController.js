5401d05a17e4708379767a34a395191738bd50cc
Lori Corbani <lori.corbani@jax.org>
2022-06-10 07:34:46 -0400

    wts883/gxd_index & other ReferenceCitationCache updates

--- pwi/static/app/edit/gxd/GxdIndexController.js
+++ pwi/static/app/edit/gxd/GxdIndexController.js
@@ -537,7 +537,7 @@
 
 			ValidateMarkerAPI.search(params, function(data) {
 				if (data.length == 0) {
-					alert("Invalid Marker Symbol\n" + vm.apiDomain.markerSymbol);
+					alert("Invalid Marker Symbol: " + vm.apiDomain.markerSymbol);
 					document.getElementById(id).focus();
 					vm.apiDomain.markerKey = "";
 					vm.apiDomain.markerSymbol = "";
