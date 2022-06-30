d170e26edb27238a3fd27623b5dd78003225d9bb
Joel Richardson <joel.richardson@jax.org>
2022-06-30 09:44:49 -0400

    (CRM-250) SQA: PWI Allele detail - phenotypes section (CRM-283) SQA: Allele detail: spinner (CRM-282) SQA: Allele detail: Ordering issues

--- pwi/static/app/edit/alleledetail/AlleleDetailService.js
+++ pwi/static/app/edit/alleledetail/AlleleDetailService.js
@@ -3,56 +3,32 @@
 	angular.module('pwi.alleledetail')
 		.factory('AlleleSearchAPI',	        AlleleSearchAPIResource)
 		.factory('AlleleGetAPI',	        AlleleGetAPIResource)
-		.factory('GenotypeSearchAPI',	        GenotypeSearchAPIResource)
-		.factory('GenotypeGetAPI',	        GenotypeGetAPIResource)
-		.factory('GenotypeMPAnnotGetAPI',       GenotypeMPAnnotGetAPIResource)
-		.factory('GenotypeDOAnnotGetAPI',       GenotypeDOAnnotGetAPIResource)
+		.factory('GenotypeGetByAlleleAPI',	GenotypeGetByAlleleAPIResource)
 		.factory('ImageGetAPI',   	        ImageGetAPIResource)
 		.factory('ImagePaneGetAPI',   	        ImagePaneGetAPIResource)
 		.factory('VocTermAncestorsGetAPI',      VocTermAncestorsGetAPIResource)
 		;
 
-	// object summary search
+	//
 	function AlleleSearchAPIResource($resource, JAVA_API_URL) {
 		return $resource(JAVA_API_URL + 'allele/search', {}, {
 			'search': { method: 'POST', isArray: true }
 		});
 	}
 
-	// object retrieval by key
+	//
 	function AlleleGetAPIResource($resource, JAVA_API_URL) {
 		return $resource(JAVA_API_URL + 'allele/:key', {}, {
 			'': { method: 'JSONP' } 
 		});
 	}
 
-	// 
-	function GenotypeSearchAPIResource($resource, JAVA_API_URL) {
-		return $resource(JAVA_API_URL + 'genotype/search', {}, {
-			'search': { method: 'POST', isArray: true }
-		});
-	}
-
-	// object retrieval by key
-	function GenotypeGetAPIResource($resource, JAVA_API_URL) {
-		return $resource(JAVA_API_URL + 'genotype/:key', {}, {
-			'': { method: 'JSONP' } 
-		});
-	}
-
 	//
-	function GenotypeMPAnnotGetAPIResource($resource, JAVA_API_URL) {
-		return $resource(JAVA_API_URL + 'genotypeMPannot/:key', {}, {
-			'': { method: 'JSONP' }
+        function GenotypeGetByAlleleAPIResource($resource, JAVA_API_URL) {
+		return $resource(JAVA_API_URL + 'genotype/getGenotypesByAllele', {}, {
+			'get': { method: 'POST', isArray: true }
 		});
-	}
-
-	//
-	function GenotypeDOAnnotGetAPIResource($resource, JAVA_API_URL) {
-		return $resource(JAVA_API_URL + 'genotypeDOannot/:key', {}, {
-			'': { method: 'JSONP' }
-		});
-	}
+        }
 
 	//
 	function ImageGetAPIResource($resource, JAVA_API_URL) {
