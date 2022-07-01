d0557f586bd9e6ea6fdcca40a8a3cc09bfe209bf
Lori Corbani <lori.corbani@jax.org>
2022-07-01 06:49:03 -0400

    mgitype_api/obsolete

--- pwi/views/api/blueprint.py
+++ pwi/views/api/blueprint.py
@@ -87,9 +87,6 @@ api.add_namespace(gxdindex_ns)
 from .gxd_genotype_api import api as gxd_genotype_api
 api.add_namespace(gxd_genotype_api)
 
-from .mgitype_api import api as mgitype_ns
-api.add_namespace(mgitype_ns)
-
 from .actualdb_api import api as actualdb_api
 api.add_namespace(actualdb_api)
 
