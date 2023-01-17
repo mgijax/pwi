533f326fd76bd9298c84915f5bcb5312153f8f7b
Joel Richardson <joel.richardson@jax.org>
2023-01-17 12:02:39 -0500

    (WTS2-888/FL2-124) PWI page revamp: Probe Summary - PWI component - delete old pages.

--- pwi/views/summary/blueprint.py
+++ pwi/views/summary/blueprint.py
@@ -7,7 +7,6 @@ summary = Blueprint('summary', __name__, url_prefix='/summary')
 
 from . import experiment_summary
 from . import gxd_summary
-from . import probe_summary
 from . import reference_summary
 from . import result_summary
 from . import sequence_summary
