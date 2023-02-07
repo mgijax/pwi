30d8661df711b12a93fefbec302238ff153c3e80
Joel Richardson <joel.richardson@jax.org>
2023-02-07 06:15:42 -0500

    (WTS2-888) Remove old result summary.

--- pwi/views/summary/blueprint.py
+++ pwi/views/summary/blueprint.py
@@ -7,6 +7,5 @@ summary = Blueprint('summary', __name__, url_prefix='/summary')
 
 from . import experiment_summary
 from . import reference_summary
-from . import result_summary
 from . import sequence_summary
 from . import specimen_summary
