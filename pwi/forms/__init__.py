15381fb11636d57ed1ad28e0ebf6ae337d25ecf6
Lori Corbani <lori.corbani@jax.org>
2023-02-14 09:57:02 -0500

    wts2-888/fl2-202/technical debt/remove experiment,sequence,probe

--- pwi/forms/__init__.py
+++ pwi/forms/__init__.py
@@ -4,7 +4,6 @@ from .gxd_query import *
 from .image_query import *
 from .imagepane_query import *
 from .marker_query import *
-from .probe_query import *
 from .reference_query import *
 from .result_query import *
 from .specimen_query import *
