ff1f75c4365442bdbfbb77d5fc0fccc37e61bff8
Lori Corbani <lori.corbani@jax.org>
2023-12-04 07:07:55 -0500

    wts2-511/fl2-540/Variant Module: automate HGVS string creation/analysis

--- pwi/static/app/edit/variant/VariantController.js
+++ pwi/static/app/edit/variant/VariantController.js
@@ -1054,7 +1054,7 @@
                                 || vm.variant.curatedGenomic.referenceSequence  == ''
                                 || vm.variant.curatedGenomic.variantSequence == ''
                                 ) {
-                                alert("HGVS requires:\n\nChromosome\nCurated Genomic Start Coordinate\nCuratored Genomic Reference Sequence\nCuratoed Genomic Variant Allele\n");
+                                alert("HGVS requires:\n\nChromosome\nCurated Genomic Start Coordinate\nCuratored Genomic Reference Allele\nCuratoed Genomic Variant Allele\n");
                                 return;
                         }
 
